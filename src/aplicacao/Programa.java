bb7ac28ac4e264c51cf24a5920ceb73d23547c90
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-07 01:16:59 -0300

    Classe Produto e teste

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import entidades.Produto;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Produto p = new Produto("TV", 1000.00);
+		
+		System.out.println(p);
+
+	}
+
+}
